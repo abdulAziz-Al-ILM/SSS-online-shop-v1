82b455e75b40530d2a5d79e5233771fb7ae16a36
abdulAziz-Al-ILM <abdulazizibnibrohim636@gmail.com>
2026-02-17 18:38:26 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,33 +1,30 @@
+
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URL
+from bson import ObjectId
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client['online_store_db']
 
-# Kolleksiyalar
 products_col = db['products']
 orders_col = db['orders']
 settings_col = db['settings']
+temp_codes = db['temp_codes'] # Login kodlari uchun
 
-async def add_product(name, price, category, stock, file_id, description, tags):
+async def add_product(name, price, stock, image_url, description, tags, category="General"):
     await products_col.insert_one({
         "name": name,
         "price": price,
-        "category": category,
         "stock": stock,
-        "file_id": file_id, # Rasmning telegramdagi IDsi
+        "image_url": image_url, # URL saqlaymiz!
         "description": description,
-        "tags": tags,
+        "tags": tags, # List ko'rinishida: ["arzon", "yangi"]
+        "category": category,
         "is_active": True
     })
 
-async def get_all_products():
-    return await products_col.find({"is_active": True}).to_list(length=100)
-
-async def update_stock(product_id, quantity):
-    # Mahsulot sonini o'zgartirish
-    await products_col.update_one({"_id": product_id}, {"$inc": {"stock": quantity}})
-
-# Do'kon sozlamalari (Logo, Video, Ranglar)
-async def update_settings(key, value):
-    await settings_col.update_one({"type": "general"}, {"$set": {key: value}}, upsert=True)
+async def get_product_by_id(pid):
+    try:
+        return await products_col.find_one({"_id": ObjectId(pid)})
+    except:
+        return None
