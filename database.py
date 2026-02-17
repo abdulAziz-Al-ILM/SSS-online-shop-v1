3e637c806613701b0dd1d4a88dce07b3788e644f
abdulAziz-Al-ILM <abdulazizibnibrohim636@gmail.com>
2026-02-17 18:47:35 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,5 @@
-
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URL
-from bson import ObjectId
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client['online_store_db']
@@ -9,22 +7,17 @@ db = client['online_store_db']
 products_col = db['products']
 orders_col = db['orders']
 settings_col = db['settings']
-temp_codes = db['temp_codes'] # Login kodlari uchun
 
-async def add_product(name, price, stock, image_url, description, tags, category="General"):
+# file_id nomini image_url deb tushuning, yoki kodda shunday qoldirib, 
+# ichiga URL yozamiz (bot.py da shunday qildik).
+async def add_product(name, price, category, stock, file_id, description, tags):
     await products_col.insert_one({
         "name": name,
         "price": price,
-        "stock": stock,
-        "image_url": image_url, # URL saqlaymiz!
-        "description": description,
-        "tags": tags, # List ko'rinishida: ["arzon", "yangi"]
         "category": category,
+        "stock": stock,
+        "image_url": file_id, # Bazaga 'image_url' nomi bilan yozamiz
+        "description": description,
+        "tags": tags,
         "is_active": True
     })
-
-async def get_product_by_id(pid):
-    try:
-        return await products_col.find_one({"_id": ObjectId(pid)})
-    except:
-        return None
