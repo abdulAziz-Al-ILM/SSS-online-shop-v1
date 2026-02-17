fe2f8b75459e54d9ed7694565541dfc920284a0c
abdulAziz-Al-ILM <abdulazizibnibrohim636@gmail.com>
2026-02-17 22:53:21 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,23 +1,25 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URL
+from bson import ObjectId
 
 client = AsyncIOMotorClient(MONGO_URL)
-db = client['online_store_db']
-
+db = client['simple_shop_db']
 products_col = db['products']
-orders_col = db['orders']
-settings_col = db['settings']
 
-# file_id nomini image_url deb tushuning, yoki kodda shunday qoldirib, 
-# ichiga URL yozamiz (bot.py da shunday qildik).
-async def add_product(name, price, category, stock, file_id, description, tags):
+async def add_product(name, price, stock, file_id, description):
     await products_col.insert_one({
         "name": name,
         "price": price,
-        "category": category,
         "stock": stock,
-        "image_url": file_id, # Bazaga 'image_url' nomi bilan yozamiz
-        "description": description,
-        "tags": tags,
-        "is_active": True
+        "file_id": file_id, # Telegram file_id
+        "description": description
     })
+
+async def get_all_products():
+    return await products_col.find().to_list(length=100)
+
+async def get_product(product_id):
+    return await products_col.find_one({"_id": ObjectId(product_id)})
+
+async def delete_product(product_id):
+    await products_col.delete_one({"_id": ObjectId(product_id)})
