0554fcef6988e23966c80b6c21bc9c4f8a680ae2
abdulAziz-Al-ILM <abdulazizibnibrohim636@gmail.com>
2026-02-17 22:43:53 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",")] # "12345,67890" kabi kiritiladi
-MONGO_URL = os.getenv("MONGO_URL") # Railwaydagi MongoDB URL
-WEBAPP_URL = os.getenv("WEBAPP_URL") # Railway domen
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",")]
+MONGO_URL = os.getenv("MONGO_URL")
+# Railway variablesga CARD_NUMBER deb qo'shasan
+CARD_NUMBER = os.getenv("CARD_NUMBER", "Karta kiritilmagan")
